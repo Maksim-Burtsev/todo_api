9fb6005273f8e99a9021db036b1ee0943b149360
MaximBurtsev <zadrot-lol@list.ru>
2022-04-19 13:21:19 +0300

    add permission

--- todo/urls.py
+++ todo/urls.py
@@ -5,7 +5,7 @@ from rest_framework import routers
 from todo.views import TodoViewSet
 
 router = routers.SimpleRouter()
-router.register(r'todo', TodoViewSet)
+router.register(r'todo', TodoViewSet, basename='todo')
 
 urlpatterns = [
     path('', include(router.urls)),
--- todo/views.py
+++ todo/views.py
@@ -1,12 +1,14 @@
+from urllib import request
 from rest_framework import viewsets
 
 from todo.models import Task
 from todo.serializers import TaskSerializer
-
+from todo.permissions import IsOwner
 
 class TodoViewSet(viewsets.ModelViewSet):
 
     serializer_class = TaskSerializer
+    permission_classes = [IsOwner]
 
     def get_queryset(self):
-        return Task.objects.all()
+        return Task.objects.filter(user=self.request.user)
