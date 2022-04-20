dea68429f67821795629f498222855a9834d737a
MaximBurtsev <zadrot-lol@list.ru>
2022-04-20 23:01:13 +0300

    add date validator and some tests

--- todo/models.py
+++ todo/models.py
@@ -1,5 +1,13 @@
+from datetime import date
+
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
+
+
+def validate_date(task_date):
+    if task_date < date.today():
+        raise ValidationError('Date can only be current or future!')
 
 
 class Task(models.Model):
@@ -18,8 +26,8 @@ class Task(models.Model):
                              on_delete=models.CASCADE, related_name='tasks', null=True)
     is_done = models.BooleanField(default=False)
     priority = models.PositiveSmallIntegerField(blank=True,
-                                null=True, choices=PRIORITY_CHOICE)
-    date = models.DateField()
+                                                null=True, choices=PRIORITY_CHOICE)
+    date = models.DateField(validators=[validate_date])
     overdue = models.BooleanField(blank=True, null=True, default=False)
 
     def __str__(self) -> str:
--- todo/tests/test_views.py
+++ todo/tests/test_views.py
@@ -15,6 +15,10 @@ class TodoTestCase(TestCase):
             username='test_user2',
             password='123ruFOJDvdspqw0id0f'
         )
+        self.user_2 = User.objects.create(
+            username='test_user_2_0',
+            password='123ruFOJDvdspqw0id0f'
+        )
         for i in range(10):
             Task.objects.create(
                 name=f'Test task{i}',
@@ -57,4 +61,80 @@ class TodoTestCase(TestCase):
         self.assertEqual(last_task.user.id, 1)
 
     def test_update(self):
-        pass
+        self.client.force_login(self.user)
+        self.task = Task.objects.get(pk=1)
+
+        url = reverse('todo-detail', args=(self.task.id,))
+        test_date = date.today()
+        data = {
+            'id': 1,
+            'name': 'Updated name',
+            'date': test_date
+        }
+
+        response = self.client.put(url, data, content_type='application/json')
+        self.task.refresh_from_db()
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(self.task.name, 'Updated name')
+        self.assertEqual(self.task.date, test_date)
+
+    def test_delete(self):
+        self.client.force_login(self.user)
+        self.task = Task.objects.get(pk=1)
+
+        tasks_count = Task.objects.all().count()
+        url = reverse('todo-detail', args=(self.task.id,))
+
+        response = self.client.delete(url)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertEqual(Task.objects.all().count(), tasks_count-1)
+
+    def test_delete_not_owner(self):
+
+        self.task = Task.objects.get(pk=3)
+
+        self.client.force_login(self.user_2)
+
+        url = reverse('todo-detail', args=(self.task.id,))
+
+        response = self.client.delete(url)
+
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_update_not_owner(self):
+        self.client.force_login(self.user_2)
+        self.task = Task.objects.get(pk=1)
+
+        url = reverse('todo-detail', args=(self.task.id,))
+        test_date = date.today()
+        data = {
+            'id': 1,
+            'name': 'Updated name',
+            'date': test_date
+        }
+
+        response = self.client.put(url, data, content_type='application/json')
+
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_get_detail(self):
+        self.client.force_login(self.user)
+        self.task = Task.objects.get(pk=1)
+
+        url = reverse('todo-detail', args=(self.task.id,))
+
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data.get('id'), self.task.id)
+
+    def test_get_detail_not_owner(self):
+        self.client.force_login(self.user_2)
+        self.task = Task.objects.get(pk=1)
+
+        url = reverse('todo-detail', args=(self.task.id,))
+
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
