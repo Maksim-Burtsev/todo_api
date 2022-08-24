569bd4f9eec344dafe14831e8fdb1ebf60f67b70
MaximBurtsev <zadrot-lol@list.ru>
2022-08-24 14:15:00 +0300

    docs: add doc strings

--- todo/services.py
+++ todo/services.py
@@ -10,13 +10,21 @@ from rest_framework import status
 
 from todo.models import Task, ResetPasswordCode
 
-def validate_and_decrement_reset_code(user_id: int, user_code):
-    """"""
+
+def validate_and_decrement_reset_code(
+    user_id: int, user_code: ResetPasswordCode
+) -> None:
+    """
+    Проверяет введённый пользователем код и уменьшает количество попыток на его правильное введение.
+    """
     correct_code = _get_correct_user_code(user_id)
     _decrement_code_attempts(correct_code, user_code)
 
-def _get_correct_user_code(user_id: int):
-    """"""
+
+def _get_correct_user_code(user_id: int) -> ResetPasswordCode:
+    """
+    Достаёт из базы правильный (ожидаемый) код восстановления от пользователя
+    """
     reset_password_code = ResetPasswordCode.objects.filter(user_id=user_id)
     if not reset_password_code.exists():
         raise serializers.ValidationError("Password wasn't reset")
@@ -26,8 +34,11 @@ def _get_correct_user_code(user_id: int):
     return correct_code
 
 
-def _decrement_code_attempts(correct_code, user_code: int) -> None:
-    """"""
+def _decrement_code_attempts(correct_code: ResetPasswordCode, user_code: int) -> None:
+    """
+    Уменьшает количество попыток (-1) на отправку кода восстановления пароля.
+    Если код неправильный/попытки исчерпаны/код просрочен, то выбрасывается ошибка.
+    """
     if correct_code.attempt == 0:
         raise APIException(
             detail="Attempts are over", status=status.HTTP_429_TOO_MANY_REQUESTS
